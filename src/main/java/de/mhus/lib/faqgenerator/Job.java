bd25fc9f6bac3e1a009a669230eb1f12e5eb8316
Mike Hummel <>
2019-10-04 15:36:45 +0200

    Update Job.java

--- src/main/java/de/mhus/lib/faqgenerator/Job.java
+++ src/main/java/de/mhus/lib/faqgenerator/Job.java
@@ -15,7 +15,7 @@ public class Job {
 
     public void addTopic(Properties context, String name) {
         topics.add(context);
-        context.put("_files", new LinkedList<>());
+        context.put("topicFiles", new LinkedList<>());
         context.put("topicName", name);
         if (!context.containsKey("topicTitle"))
             context.put("topicTitle", name);
@@ -28,7 +28,7 @@ public class Job {
         if (topicName == null) return;
         for (Properties topic : topics) {
             if (topicName.equals(topic.get("topicName"))) {
-                ((LinkedList)topic.get("_files")).add(context);
+                ((LinkedList)topic.get("topicFiles")).add(context);
                 break;
             }
         }
